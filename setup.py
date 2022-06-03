7164b1c35e8da008840f9ed1957ecdbb73fa5be8
mamthamv <44310658+mamthamv@users.noreply.github.com>
2022-06-03 23:48:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,28 @@ except ImportError:
 
 setup(
     name='tr-awsbackup-infra',
-    version='0.0.2',
-    url='https://github.com/mamthamv/version_tag.git'
+    version='0.0.3',
+    description='Tool to manage AWS Backups', 
+    long_description_content_type="text/markdown",
+    author='Nuvola',
+    author_email='DataServicesTeam1@thomson.com',
+    url='https://github.com/tr/nuvola_aws-backup-infra-cdk/',
+    packages=find_packages(),
+    package_data={'': ['*.config', '*.json', '*.cfg', '*.yaml']},
+    include_package_data=True,
+    install_requires=[
+        'aws-cdk-lib==2.19.0', 
+        'constructs',   
+        'pyyaml',   
+        'tr-aws-cdk.core==2.0.2'
+    ],
+    setup_requires=[
+        'flake8',
+        'sphinx',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
